9ce7716749f30cda5d071cf2a21641b95ee479c8
Marcin Michalik <terragady@gmail.com>
2021-02-15 17:21:51 +0100

    instagram followers

--- src/App.js
+++ src/App.js
@@ -5,30 +5,36 @@ import config from './config';
 document.title = "Yallow Life Science - DashBoard";
 
 const url = `https://sheets.googleapis.com/v4/spreadsheets/${config.spreadsheetId}/values:batchGet?ranges=Sheet1!A:B&majorDimension=COLUMNS&ranges=Sheet1!D:N&ranges=Sheet1!P:Y&ranges=Sheet1!AB:AF&key=${config.apiKey}`;
-
+const instaUrl = 'https://www.instagram.com/yallowlifescience/channel/?__a=1';
 
 
 function App() {
   const [sheet, setSheet] = useState([]);
+  const [insta, setInsta] = useState({foll: 0, posts: 0});
 
-
+  
   const fetchFromSheets = () => {
   fetch(url).then(response => response.json()).then(data => {
-        setSheet(data);
-        console.log("drugie");
+    setSheet(data);
+  })
+}
+
+const fetchFromInstagram = () => {
+  fetch(instaUrl).then(response => response.json()).then(data => {
+    setInsta({foll: data.graphql.user.edge_followed_by.count, posts: data.graphql.user.edge_owner_to_timeline_media.count});
       })
 }
   useEffect(() => {
-    console.log("pierwsze");
     fetchFromSheets();
+    fetchFromInstagram();
     const interval = setInterval(() => {
       fetchFromSheets();
-    }, 10000);
+      fetchFromInstagram();
+    }, 300000);
 
     return () => clearInterval(interval)
   }, [])
-
-
+console.log(sheet)
 
   return (
     <div className="App">
@@ -37,6 +43,8 @@ function App() {
         <p>
           Yallow dashboard will be here!
         </p>
+        Instagram Followers: {insta.foll} <br />
+        Instagram Posts: {insta.posts} <br />
         { sheet && sheet.valueRanges ? JSON.stringify(sheet.valueRanges[0]) : "Loading..." }
 
       </header>

2b6a4756e4add39e5a4a4bc25cce4b2312a31dc9
Marcin Michalik <terragady@gmail.com>
2021-02-15 19:39:06 +0100

    hello

--- src/App.js
+++ src/App.js
@@ -1,29 +1,71 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import { Bar } from 'react-chartjs-2'
+import { defaults } from 'react-chartjs-2';
 import './App.css';
 import config from './config';
+
 document.title = "Yallow Life Science - DashBoard";
 
 const url = `https://sheets.googleapis.com/v4/spreadsheets/${config.spreadsheetId}/values:batchGet?majorDimension=COLUMNS&ranges=Sheet1!D:N&ranges=Sheet1!P:Y&ranges=Sheet1!AB:AF&key=${config.apiKey}`;
 const instaUrl = 'https://www.instagram.com/yallowlifescience/channel/?__a=1';
-
+const options = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+}
 
 function App() {
-  const [sheet, setSheet] = useState([]);
-  const [insta, setInsta] = useState({foll: 0, posts: 0});
+  const [sheet, setSheet] = useState({
+    hours: {
+      labels: [],
+      datasets: [{
+        label: "Petter",
+        data: []
+      }]
+    }
+  });
+  const [insta, setInsta] = useState({ foll: 0, posts: 0 });
 
-  
+
   const fetchFromSheets = () => {
-  fetch(url).then(response => response.json()).then(data => {
-    setSheet(data);
-  })
-}
+    fetch(url).then(response => response.json()).then(data => {
+      let weekLabels = [];
+      let datasets = [];
 
-const fetchFromInstagram = () => {
-  fetch(instaUrl).then(response => response.json()).then(data => {
-    setInsta({foll: data.graphql.user.edge_followed_by.count, posts: data.graphql.user.edge_owner_to_timeline_media.count});
-      })
-}
+      data.valueRanges[1].values.forEach((e, i) => {
+        if (i === 0) {
+          e.forEach((e, i) => {
+            if (i === 0) { return }
+            weekLabels = [...weekLabels, e];
+          })
+        } else {
+          let tempData = { label: "", data: [], backgroundColor: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})` }
+          e.forEach((e, i) => {
+            if (i === 0) { tempData.label = e } else {
+              tempData['data'].push(e);
+            }
+            
+          })
+          datasets = [...datasets, tempData]
+        }
+      });
+
+      // data.valueRanges[1].values[0]
+      setSheet({ hours: { labels: weekLabels, datasets } });
+    })
+  }
+
+  const fetchFromInstagram = () => {
+    fetch(instaUrl).then(response => response.json()).then(data => {
+      setInsta({ foll: data.graphql.user.edge_followed_by.count, posts: data.graphql.user.edge_owner_to_timeline_media.count });
+    })
+  }
   useEffect(() => {
     fetchFromSheets();
     fetchFromInstagram();
@@ -34,18 +76,17 @@ const fetchFromInstagram = () => {
 
     return () => clearInterval(interval)
   }, [])
-console.log(sheet)
+  console.log(sheet)
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Yallow dashboard will be here!
         </p>
         Instagram Followers: {insta.foll} <br />
         Instagram Posts: {insta.posts} <br />
-        { sheet && sheet.valueRanges ? JSON.stringify(sheet.valueRanges[0].values[0]) : "Loading..." }
+        <Bar data={sheet.hours} options={options} />
 
       </header>
     </div>

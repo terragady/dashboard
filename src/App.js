c73ef4c2c3eab58641b748c3a3d2c7de34e5f7a1
Marcin Michalik <terragady@gmail.com>
2021-02-15 16:04:35 +0100

    added google api support

--- src/App.js
+++ src/App.js
@@ -2,18 +2,32 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import config from './config';
+document.title = "Yallow Life Science - DashBoard";
 
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${config.spreadsheetId}/values:batchGet?ranges=Sheet1!A:B&majorDimension=COLUMNS&ranges=Sheet1!D:N&ranges=Sheet1!P:Y&ranges=Sheet1!AB:AF&key=${config.apiKey}`;
 
-const url = `https://sheets.googleapis.com/v4/spreadsheets/${ config.spreadsheetId }/values:batchGet?ranges=Sheet1&majorDimension=ROWS&key=${ config.apiKey }`;
 
 
 function App() {
   const [sheet, setSheet] = useState([]);
+
+
+  const fetchFromSheets = () => {
+  fetch(url).then(response => response.json()).then(data => {
+        setSheet(data);
+        console.log("drugie");
+      })
+}
   useEffect(() => {
-    fetch(url).then(response => response.json()).then(data => {
-    setSheet(data);
-  })
+    console.log("pierwsze");
+    fetchFromSheets();
+    const interval = setInterval(() => {
+      fetchFromSheets();
+    }, 10000);
+
+    return () => clearInterval(interval)
   }, [])
+
 
 
   return (
@@ -23,7 +37,7 @@ function App() {
         <p>
           Yallow dashboard will be here!
         </p>
-        {JSON.stringify(sheet)}
+        { sheet && sheet.valueRanges ? JSON.stringify(sheet.valueRanges[0]) : "Loading..." }
 
       </header>
     </div>

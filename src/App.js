3f658846b2db7b017936c741c70e145d95275756
Marcin Michalik <terragady@gmail.com>
2021-02-15 14:46:43 +0100

    added google api support

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import config from './config';
+
+
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${ config.spreadsheetId }/values:batchGet?ranges=Sheet1&majorDimension=ROWS&key=${ config.apiKey }`;
+
 
 function App() {
+  const [sheet, setSheet] = useState([]);
+  useEffect(() => {
+    fetch(url).then(response => response.json()).then(data => {
+    setSheet(data);
+  })
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +23,7 @@ function App() {
         <p>
           Yallow dashboard will be here!
         </p>
+        {JSON.stringify(sheet)}
 
       </header>
     </div>
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  apiKey: 'AIzaSyAhJlD7G3G5EWwHSQWJA-oiY3rm_9mr2P8',
+  spreadsheetId: '1VUxJ_qQwCoPou2nzBn2B-yPmN2ICeHzfT6FQvbMjOL8'
+}

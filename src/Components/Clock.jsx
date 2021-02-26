cb16458818b8e1a9b739fa2a1fea34506cb84aee
Marcin Michalik <terragady@gmail.com>
2021-02-26 16:46:52 +0100

    fix the current week number

--- src/Components/Clock.jsx
+++ src/Components/Clock.jsx
@@ -17,8 +17,8 @@ function Clock() {
 
   function currentWeek(date) {
     const oneJan = new Date(date.getFullYear(), 0, 1);
-    const numberOfDays = Math.floor((date - oneJan) / (24 * 60 * 60 * 1000));
-    return Math.ceil((date.getDay() + 1 + numberOfDays) / 7)
+    const numberOfDays = ((date - oneJan) / (24 * 60 * 60 * 1000));
+    return numberOfDays>(7-oneJan.getDay()) ? Math.ceil((numberOfDays - (7-oneJan.getDay())) / 7) : "yes"
   }
   return (
     <>

3675610d374288720076c5bd91a0d792f34bc092
Marcin Michalik <terragady@gmail.com>
2021-05-03 13:35:13 +0200

    fixed instagram fetching and added new employee

--- src/App.js
+++ src/App.js
@@ -14,12 +14,11 @@ import Clock from './Components/Clock';
 
 document.title = "Yallow Life Science - DashBoard";
 
-const url = `https://sheets.googleapis.com/v4/spreadsheets/1VUxJ_qQwCoPou2nzBn2B-yPmN2ICeHzfT6FQvbMjOL8/values:batchGet?majorDimension=COLUMNS&ranges=Sheet1!D:N&ranges=Sheet1!P:Y&ranges=Sheet1!AB:AF&ranges=Sheet1!B2&ranges=Sheet1!B4&key=AIzaSyAhJlD7G3G5EWwHSQWJA-oiY3rm_9mr2P8`;
-const instaUrl = 'https://www.instagram.com/yallowlifescience/channel/?__a=1';
+const url = `https://sheets.googleapis.com/v4/spreadsheets/1VUxJ_qQwCoPou2nzBn2B-yPmN2ICeHzfT6FQvbMjOL8/values:batchGet?majorDimension=COLUMNS&ranges=Sheet1!D:N&ranges=Sheet1!P:Z&ranges=Sheet1!AB:AF&ranges=Sheet1!B2&ranges=Sheet1!B4&ranges=Sheet1!B5&ranges=Sheet1!B6&key=AIzaSyAhJlD7G3G5EWwHSQWJA-oiY3rm_9mr2P8`;
 function App() {
   const [sheet, setSheet] = useState({});
   const [overallAvg, setOverallAvg] = useState({});
-  const [insta, setInsta] = useState({ foll: 0, posts: 0 });
+  const [insta, setInsta] = useState(0);
   const [linked, setLinked] = useState(0);
   const [diffData, setDiffData] = useState(0);
   const [finance, setFinance] = useState(0);
@@ -30,6 +29,8 @@ function App() {
 
   const fetchFromSheets = () => {
     fetch(url).then(response => response.json()).then(data => {
+
+      // how much weeks behind fetch data should occur
       const weeksBehind = 25
       // fetch hours
       let datasets = [];
@@ -38,12 +39,12 @@ function App() {
         // how many weeks behind
         if (i === 0 || i < data.valueRanges[1].values[0].length - weeksBehind) { return }
         for (let l = 1; l < data.valueRanges[1].values.length; l++) {
-          // skip the names stated here
+          // skip the names stated here or do something with them
           if (data.valueRanges[1].values[l][0] === "Overall Avg. IR") {
             NewOverallAvg.push({ week: data.valueRanges[1].values[0][i], name: data.valueRanges[1].values[l][0], value: parseFloat(data.valueRanges[1].values[l][i]), value1: parseFloat(data.valueRanges[1].values[l][i]) })
           }
-          else if (data.valueRanges[1].values[l][0] === "Employee 7") {
-          } else {
+          else if (data.valueRanges[1].values[l][0] === "Employee 7") {}
+          else {
             datasets.push({ week: data.valueRanges[1].values[0][i], name: data.valueRanges[1].values[l][0], value: parseFloat(data.valueRanges[1].values[l][i]), avg: parseFloat(data.valueRanges[1].values[l][i]) })
           }
         }
@@ -82,8 +83,6 @@ function App() {
         }
       }
       )
-      console.log(NEW_Finance)
-
 
       //fetch diff data
       let instaNewFollPosts = []
@@ -123,24 +122,16 @@ function App() {
       if (JSON.stringify(finance) !== JSON.stringify(NEW_Finance)) { setFinance(NEW_Finance); }
       if (JSON.stringify(diffData) !== JSON.stringify({ instaNewFollPosts, linkedNewFollPosts })) { setDiffData({ instaNewFollPosts, linkedNewFollPosts }) }
       if (JSON.stringify(linked) !== JSON.stringify({ foll: data.valueRanges[3].values[0], posts: data.valueRanges[4].values[0] })) { setLinked({ foll: data.valueRanges[3].values[0], posts: data.valueRanges[4].values[0] }) }
+      if (JSON.stringify(insta) !== JSON.stringify({ foll: data.valueRanges[5].values[0], posts: data.valueRanges[6].values[0] })) { setInsta({ foll: data.valueRanges[5].values[0], posts: data.valueRanges[6].values[0] }) }
       if (JSON.stringify(sheet) !== JSON.stringify(datasets)) { setSheet(datasets) }
       if (JSON.stringify(overallAvg) !== JSON.stringify(NewOverallAvg)) { setOverallAvg(NewOverallAvg) }
     })
   }
 
-  const fetchFromInstagram = () => {
-    fetch(instaUrl).then(response => response.json()).then(data => {
-      if (insta.foll !== data.graphql.user.edge_followed_by.count || insta.posts !== data.graphql.user.edge_owner_to_timeline_media.coun) {
-        setInsta({ foll: data.graphql.user.edge_followed_by.count, posts: data.graphql.user.edge_owner_to_timeline_media.count });
-      }
-    }).catch(err => console.log("Fetch Error: " + err))
-  }
   useEffect(() => {
     fetchFromSheets();
-    fetchFromInstagram();
     const interval = setInterval(() => {
       fetchFromSheets();
-      fetchFromInstagram();
     },
       // 5000
       3600000
